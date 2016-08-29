6562f2317199603e295af970f08f954e80f53d00
Mike Gering <mike@gattishouse.com>
2016-08-29 19:07:42 -0400

    restructuring

--- __init__.py
+++ __init__.py
@@ -1,12 +1,52 @@
+bl_info = {
+    "name": "New Wind Spinner",
+    "author": "Mike Gering",
+    "version": (1, 0),
+    "blender": (2, 75, 0),
+    "location": "View3D > Add > Mesh > New Wind Spinner",
+    "description": "Adds a new Wind Spinner Mesh Object",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Add Mesh",
+    }
+print("initing")
 import bpy
 from bpy.types import Operator
 from bpy.props import FloatVectorProperty, FloatProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
 
-class AddWindSpinner(bpy.types.Operator):
+def add_object(self, context):
+    scale_x = self.scale.x
+    scale_y = self.scale.y
+
+    verts = [Vector((-1 * scale_x, 1 * scale_y, 0)),
+             Vector((1 * scale_x, 1 * scale_y, 0)),
+             Vector((1 * scale_x, -1 * scale_y, 0)),
+             Vector((-1 * scale_x, -1 * scale_y, 0)),
+            ]
+
+    edges = []
+    faces = [[0, 1, 2, 3]]
+
+    mesh = bpy.data.meshes.new(name="New Wind Spinner")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+
+class AddWindSpinner(bpy.types.Operator, AddObjectHelper):
     """Add Wind Spinner"""
-    bl_idname = 'object.add_wind_spinner'
+    bl_idname = 'mesh.add_wind_spinner'
     bl_label = 'Add Wind Spinner'
     bl_options = {'REGISTER', 'UNDO'}
+
+    scale = FloatVectorProperty(
+            name="scale",
+            default=(1.0, 1.0, 1.0),
+            subtype='TRANSLATION',
+            description="scaling",
+            )
 
     controller_radius = FloatProperty(
     	name='controller radius',
@@ -16,13 +56,10 @@ class AddWindSpinner(bpy.types.Operator):
     	description='Radius of the spinner rotation controller')
 
     def execute(self, context):
-        #TODO: Finish
-        print("Do it!")
+        add_object(self, context)
         return {"FINISHED"}
 
-    @classmethod
-    def poll(cls, context):
-    	return True
+# Registration
 
 def add_object_button(self, context):
     self.layout.operator(
@@ -32,7 +69,11 @@ def add_object_button(self, context):
 
 def register():  
     bpy.utils.register_class(AddWindSpinner)
-    bpy.types.VIEW3D_MT_object.append(add_object_button)
-  
+    bpy.types.INFO_MT_mesh_add.append(add_object_button)
+
+def unregister():
+    bpy.utils.unregister_class(AddWindSpinner)
+    bpy.types.INFO_MT_mesh_add.remove(add_object_button)
+
 if __name__ == "__main__":  
     register()  
