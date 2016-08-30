4ba8b1733e9a08bb9fe39c90d64004560fb058ee
Mike Gering <mike@gattishouse.com>
2016-08-30 13:59:22 -0400

    ?

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+### NOTES
+# Look at pingpong example for drawing a button on a panel. Use the button execute() to create the spinner.
+# https://wiki.blender.org/index.php/Dev:Py/Scripts/Cookbook/Panels_and_Operators/Ping_Pong
+
 bl_info = {
     "name": "New Wind Spinner",
     "author": "Mike Gering",
@@ -9,14 +13,17 @@ bl_info = {
     "wiki_url": "",
     "category": "Add Mesh",
     }
-print("initing")
+
 import bpy
 from bpy.types import Operator
-from bpy.props import FloatVectorProperty, FloatProperty
+from bpy.props import FloatVectorProperty, FloatProperty, BoolProperty
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 from mathutils import Vector
 
 def add_object(self, context):
+    scene = context.scene
+    obj_act = scene.objects.active
+
     scale_x = self.scale.x
     scale_y = self.scale.y
 
@@ -55,9 +62,32 @@ class AddWindSpinner(bpy.types.Operator, AddObjectHelper):
     	unit='LENGTH',
     	description='Radius of the spinner rotation controller')
 
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column(align=True)
+        row = col.row(align=True)
+        row.prop(context.scene, 'controller_radius')
+        row = col.row(align=True)
+        row.operator('mesh.add_wind_spinner_execute')
+
     def execute(self, context):
         add_object(self, context)
         return {"FINISHED"}
+
+    ##### POLL #####
+    @classmethod
+    def poll(cls, context):
+        return context.scene != None
+
+class AddWindSpinnerExecute(bpy.types.Operator):
+    """Operator to execute spinner addition"""
+    bl_idname = 'mesh.add_wind_spinner_execute'
+    bl_label = 'Execute Add Wind Spinner'
+    #bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        print("Execute spinner add")
+        return {'FINISHED'}
 
 # Registration
 
@@ -70,10 +100,20 @@ def add_object_button(self, context):
 def register():  
     bpy.utils.register_class(AddWindSpinner)
     bpy.types.INFO_MT_mesh_add.append(add_object_button)
+    bpy.utils.register_class(AddWindSpinnerExecute)
+    bpy.types.Scene.controller_radius =  FloatProperty(
+        name='controller radius',
+        default=0.10,
+        subtype="DISTANCE",
+        unit='LENGTH',
+        description='Radius of the spinner rotation controller')
+
 
 def unregister():
     bpy.utils.unregister_class(AddWindSpinner)
     bpy.types.INFO_MT_mesh_add.remove(add_object_button)
+    bpy.utils.unregister_class(AddWindSpinnerExecute)
+    del bpy.types.Scene.controller_radius
 
 if __name__ == "__main__":  
     register()  
